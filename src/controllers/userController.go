89f56764a04493384b0ae5945f68072f1cbcbd2e
gmldnjs26 <imleesky265@gmail.com>
2022-05-05 15:23:57 +0900

    create sortedset data of revenue ranking2

--- src/controllers/userController.go
+++ src/controllers/userController.go
@@ -1,9 +1,12 @@
 package controllers
 
 import (
+	"context"
+
 	"ambassador/src/database"
 	"ambassador/src/models"
 
+	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -13,4 +16,39 @@ func Ambassadors(c *fiber.Ctx) error {
 	database.DB.Where("is_ambassador = true").Find(&users)
 
 	return c.JSON(users)
+}
+
+func Rankings(c *fiber.Ctx) error {
+	// var users []models.User
+
+	// database.DB.Find(&users, models.User{
+	// 	IsAmbassador: true,
+	// })
+
+	// var result []interface{}
+
+	// for _, user := range users {
+	// 	ambassador := models.Ambassador(user)
+	// 	ambassador.CalculateRevenue(database.DB)
+
+	// 	result = append(result, fiber.Map{
+	// 		user.GetName(): ambassador.Revenue,
+	// 	})
+	// }
+
+	rankings, err := database.Cache.ZRevRangeByScoreWithScores(context.Background(), "rankings", &redis.ZRangeBy{
+		Min: "-inf",
+		Max: "+inf",
+	}).Result()
+	if err != nil {
+		return err
+	}
+
+	result := make(map[string]float64)
+
+	for _, ranking := range rankings {
+		result[ranking.Member.(string)] = ranking.Score
+	}
+
+	return c.JSON(result)
 }

5c0f1de9d9a308f1f1dd25ab70f492a4fc3596ec
gmldnjs26 <imleesky265@gmail.com>
2022-05-06 22:53:56 +0900

    feat: connect stripe and add complete order

--- src/controllers/orderController.go
+++ src/controllers/orderController.go
@@ -1,10 +1,15 @@
 package controllers
 
 import (
+	"context"
+
 	"ambassador/src/database"
 	"ambassador/src/models"
 
 	"github.com/gofiber/fiber/v2"
+
+	"github.com/stripe/stripe-go/v72"
+	"github.com/stripe/stripe-go/v72/checkout/session"
 )
 
 func Orders(c *fiber.Ctx) error {
@@ -75,6 +80,8 @@ func CreateOrder(c *fiber.Ctx) error {
 		})
 	}
 
+	var lineItems []*stripe.CheckoutSessionLineItemParams
+
 	for _, requestProduct := range request.Products {
 		product := models.Product{}
 		product.Id = uint(requestProduct["product_id"])
@@ -99,11 +106,97 @@ func CreateOrder(c *fiber.Ctx) error {
 				"message": err.Error(),
 			})
 		}
+
+		// For Stripe LineItems
+		lineItems = append(lineItems, &stripe.CheckoutSessionLineItemParams{
+			Name:        stripe.String(product.Title),
+			Description: stripe.String(product.Description),
+			Images:      []*string{stripe.String(product.Image)},
+			Amount:      stripe.Int64(100 * int64(product.Price)),
+			Currency:    stripe.String("usd"),
+			Quantity:    stripe.Int64(int64(requestProduct["quantity"])),
+		})
 	}
+
+	// Stripe Payment Start
+	stripe.Key = "sk_test_51KvyLfCXj5HN8X9OG0WQD9JH5Apwz7LFQjvIYeG8eMCnUS1bA6UUhMZtIiwebnnCHAhIx2rLJtthjJlOwQ3sLKam00FnCljC4r"
+
+	params := stripe.CheckoutSessionParams{
+		SuccessURL:         stripe.String("http://localhost:5000/success?source={CHECKOUT_SESSION_ID}"),
+		CancelURL:          stripe.String("http://localhost:5000/error"),
+		PaymentMethodTypes: stripe.StringSlice([]string{"card"}),
+		LineItems:          lineItems,
+	}
+
+	source, err := session.New(&params)
+	if err != nil {
+		tx.Rollback()
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+	order.TransactionId = source.ID
+	if err := tx.Save(&order).Error; err != nil {
+		tx.Rollback()
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+	// Stripe Payment Over
 
 	tx.Commit()
 
 	return c.JSON(order)
+}
+
+func CompleteOrder(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	order := models.Order{}
+
+	database.DB.Preload("OrderItems").First(&order, models.Order{
+		TransactionId: data["source"],
+	})
+
+	if order.Id == 0 {
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": "Order not found",
+		})
+	}
+
+	order.Complete = true
+	database.DB.Save(&order)
+
+	// 1.주문을 완료했음으로 랭킹데이터에 링크를 만든 유저(엠버서더)의 price를 높여주는 처리
+	// 2.email송신
+	// 3.admin ambassador notice this order is completed
+	go func(order models.Order) {
+		ambassadorRevenue := 0.0
+		adminRevenue := 0.0
+
+		for _, item := range order.OrderItems {
+			ambassadorRevenue += item.AmbassadorRevenue
+			adminRevenue += item.AdminRevenue
+		}
+
+		user := models.User{}
+		user.Id = order.UserId
+
+		database.DB.First(&user)
+
+		database.Cache.ZIncrBy(context.Background(), "rankings", ambassadorRevenue, user.GetName())
+	}(order)
+
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
 }
 
 // func DeleteOrder(c *fiber.Ctx) error {

02e077768d89088bae3a03b42b80c8a27df5b741
gmldnjs26 <imleesky265@gmail.com>
2022-05-07 14:44:41 +0900

    feat: send mail

--- src/controllers/orderController.go
+++ src/controllers/orderController.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	"net/smtp"
 
 	"ambassador/src/database"
 	"ambassador/src/models"
@@ -192,6 +194,14 @@ func CompleteOrder(c *fiber.Ctx) error {
 		database.DB.First(&user)
 
 		database.Cache.ZIncrBy(context.Background(), "rankings", ambassadorRevenue, user.GetName())
+
+		ambassadorMessage := []byte(fmt.Sprintf("You earned $%f from the link #%s", ambassadorRevenue, order.Code))
+
+		smtp.SendMail("host.docker.internal:1025", nil, "no-reply@email.com", []string{order.AmbassadorEmail}, ambassadorMessage)
+
+		adminMessage := []byte(fmt.Sprintf("Order #%d with a total of $%f has been completed", order.Id, adminRevenue))
+
+		smtp.SendMail("host.docker.internal:1025", nil, "no-reply@email.com", []string{order.AmbassadorEmail}, adminMessage)
 	}(order)
 
 	return c.JSON(fiber.Map{

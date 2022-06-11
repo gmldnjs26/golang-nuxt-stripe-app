c9fc642d3511e73ca622596566c04a3ba68f9c2e
gmldnjs26 <imleesky265@gmail.com>
2022-06-11 18:06:51 +0900

    chore: add comment

--- backend/main.go
+++ backend/main.go
@@ -19,6 +19,7 @@ func main() {
 	app.Use(cors.New(cors.Config{
 		AllowCredentials: true,
 		// TODO: 좀더 조사
+		// FIXME: 지금은 Access-Control-Allow-Origin:* 상태이다. 고치자
 		// cookie의 정보를 허용된 origin에서는 취득할 수 있게끔 설정? 응답헤더에 담아서 보내는건데 프론트에서 이 쿠키를 취득할 수 있게끔 한다?
 		// 근데 httpOnly 쿠키인데도 프론트에서 취득할 수 있나? 취득이라기 보다는 request 헤더에 담을 수 있게끔 하는 설정인가?
 	}))

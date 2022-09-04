cb9fafa14b3acb07bb5b6b216b8b7d815982d8fa
gmldnjs26 <imleesky265@gmail.com>
2022-09-04 16:29:35 +0900

    modify: set cors allow url

--- backend/main.go
+++ backend/main.go
@@ -17,9 +17,9 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:5000, http://localhost:3000, http://localhost:7777",
 		AllowCredentials: true,
 		// TODO: 좀더 조사
-		// FIXME: 지금은 Access-Control-Allow-Origin:* 상태이다. 고치자
 		// cookie의 정보를 허용된 origin에서는 취득할 수 있게끔 설정? 응답헤더에 담아서 보내는건데 프론트에서 이 쿠키를 취득할 수 있게끔 한다?
 		// 근데 httpOnly 쿠키인데도 프론트에서 취득할 수 있나? 취득이라기 보다는 request 헤더에 담을 수 있게끔 하는 설정인가?
 		// httpOnly는 javascript로 쿠키로 접근 못하게끔 하는 설정이다. 프론트에서 쿠키를 취득할 필요는 없고
